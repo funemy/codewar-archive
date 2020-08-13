480bad4bae2fb6a3b028280c5c08f4c7eb2b0b08
funemy <liyzunique@gmail.com>
2020-08-13 01:49:01 -0500

    finish pass1

--- three_pass_compiler.ml
+++ three_pass_compiler.ml
@@ -1,3 +1,5 @@
+let print_list l = let open Printf in List.iter (printf "%s ") l; print_newline();;
+
 let tokenize code =
   let rec explode string =
     if String.length string = 0 then []
@@ -43,26 +45,86 @@ exception CompilerError of string
 module type COMPILER =
 sig
   val pass1: string -> ast
-  val pass2: ast -> ast
-  val codeGen: ast -> string list
-  val compile: string -> string list 
+  (* val pass2: ast -> ast *)
+  (* val codeGen: ast -> string list *)
+  (* val compile: string -> string list  *)
 end;;
 
 
 module Compiler : COMPILER =
 struct
   let pass1 code = 
-    raise (CompilerError "missing implementation")
+    let token = tokenize code in
+    (* parse argument list *)
+    let rec parse_args tok res = match tok with 
+      | "]" :: t -> t, List.rev res
+      | "[" :: t -> parse_args t res
+      | h :: t -> parse_args t (h :: res)
+      | _ -> raise (CompilerError "parse arg list error") in
+    let prog, args = parse_args token [] in
+    print_list args;
+    (* find argument index *)
+    let arg_no s =
+      let rec aux s i = function
+        | [] -> raise (CompilerError ("PARSING ERROR: unbound argument " ^ s))
+        | h :: t -> if s = h then i else aux s (i+1) t
+      in aux s 0 args in
+    (* process operators *)
+    let process_op factor op = 
+      (* we only have bi-operators *)
+      let f1, f2, t = match factor with
+        | f1 :: f2 :: t -> f1, f2, t
+        | _ -> raise (CompilerError ("PARSING ERROR: not enough operand for " ^ op)) in 
+      let tem = match op with
+        | "+" -> Add (f2, f1) 
+        | "-" -> Sub (f2, f1) 
+        | "*" -> Mul (f2, f1) 
+        | "/" -> Div (f2, f1)
+        | _ -> raise (CompilerError ("PARSING ERROR: unsupported operator " ^ op)) in
+      tem :: t in
+    (* process the program inside a pair of parentheses *)
+    let rec process_paren factor ops = match ops with
+      | "(" :: t -> factor, t
+      | h :: t -> process_paren (process_op factor h) t
+      | _ -> raise (CompilerError "PARSING ERROR: parentheses mismatch") in
+    (* process all * and / operators on the top of the stack *)
+    let rec process_term factor ops = match ops with 
+      | "*" :: t -> process_term (process_op factor "*") t
+      | "/" :: t -> process_term (process_op factor "/") t
+      | ops -> factor, ops in
+    let rec process_all factor ops = 
+      match ops with
+      | h :: t -> process_all (process_op factor h) t
+      | [] -> (match factor with
+          | [f] -> f
+          | _ -> raise (CompilerError "PARSING ERROR: incomplete program")) in
+    let rec parse p factor ops = match p with
+      | "(" :: t -> parse t factor ("(" :: ops)
+      | ")" :: t -> let f, o = process_paren factor ops in parse t f o
+      | "+" :: t -> let f, o = process_term factor ops in parse t f ("+" :: o)
+      | "-" :: t -> let f, o = process_term factor ops in parse t f ("-" :: o)
+      | "*" :: t -> parse t factor ("*" :: ops)
+      | "/" :: t -> parse t factor ("/" :: ops)
+      | h :: t -> (match int_of_string_opt h with
+          | None -> parse t ((Arg (arg_no h)) :: factor) ops
+          | Some i -> parse t ((Imm i) :: factor) ops)
+      | [] -> process_all factor ops
+    in parse prog [] []
 
-  let pass2 ast =
-    raise (CompilerError "missing implementation")  
+  (* let pass2 ast =
+     raise (CompilerError "missing implementation")   *)
 
-  let codeGen = 
-    raise (CompilerError "missing implementation")
+  (* let codeGen = 
+     raise (CompilerError "missing implementation") *)
 
-  let compile code =
-    codeGen(pass2(pass1 code))
+  (* let compile code =
+     codeGen(pass2(pass1 code)) *)
 end;;
+
+let test_pass1 = Compiler.pass1 "[ x y ] 2 / y + x"
+let test_pass1 = Compiler.pass1 "[ x y ] ( x + y ) / 2"
+
+(* let test_pass1 = Compiler.pass1 "[ x y ] ( x + y ) / 2" *)
 
 let rec simualte : string list * int list -> int =
   let stack = Stack.create () in
